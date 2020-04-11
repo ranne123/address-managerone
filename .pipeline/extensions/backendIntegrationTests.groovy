46c225d7e46884514f5ba134625568ddc4b92474
ranne123 <ramakria999@gmail.com>
2020-04-11 16:11:14 +1000

    update
    update

--- .pipeline/extensions/backendIntegrationTests.groovy
+++ .pipeline/extensions/backendIntegrationTests.groovy
@@ -15,7 +15,7 @@ void call(Map params) {
  //  mavenExecute script: this, goals: 'clean install'
   
   echo "Executing the mvn clean verify  ..."
-  mavenExecute script: this, goals: '-am -pl :testing compile'
+  mavenExecute script: this, goals: 'clean install'
   //execute original stage as defined in the template
  // params.originalStage()
 
