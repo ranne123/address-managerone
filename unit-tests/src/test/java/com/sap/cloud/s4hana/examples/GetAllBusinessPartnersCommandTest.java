c4ca1d148891fca891816970393cc2bc04ecaadb
ranne123 <ramakria999@gmail.com>
2020-04-12 13:21:56 +1000

    corrceted the imports statement

--- unit-tests/src/test/java/com/sap/cloud/s4hana/examples/GetAllBusinessPartnersCommandTest.java
+++ unit-tests/src/test/java/com/sap/cloud/s4hana/examples/GetAllBusinessPartnersCommandTest.java
@@ -7,7 +7,7 @@ import com.sap.cloud.s4hana.examples.addressmgr.commands.GetAllBusinessPartnersC
 import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerSelectable;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.selectable.*;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import com.sap.cloud.sdk.testutil.MockUtil;
 import org.junit.Before;
