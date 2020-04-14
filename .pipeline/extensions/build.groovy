4e041735873b07f0d75d8e1bcd4f55de79ec4231
ranne123 <ramakria999@gmail.com>
2020-04-14 19:24:25 +1000

    update
    update

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -1,1 +1,16 @@
+void call(Map params) {
+  //access stage name
+  echo "Start rama - Extension for stage: ${params.stageName}"
 
+  //access config
+  echo "Current stage config: ${params.config}"
+   
+ 
+  //execute original stage as defined in the template
+ params.originalStage()
+
+  
+
+  echo "End - Extension for stage: ${params.stageName}"
+}
+return this
