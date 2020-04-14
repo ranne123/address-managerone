1da47751eecde45b37d2bfa3f5ef5ec35be6b295
ranne123 <ramakria999@gmail.com>
2020-04-15 06:58:49 +1000

    Update build groovy
    Corrected spelling mistake

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -6,7 +6,7 @@ void call(Map params) {
   echo "Current stage config: ${params.config}"
    
   //install assemblies in jenkins local repo
-  evho "install assemblies - integration-tests *******"
+  echo "install assemblies - integration-tests *******"
   mavenExecute script: this, goals: 'clean assembly:single'
   //execute original stage as defined in the template
  params.originalStage()
