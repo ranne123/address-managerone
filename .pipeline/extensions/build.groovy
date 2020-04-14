488b1cb1b079fef5b952d3dfd6cb7ebbbe64b14e
ranne123 <ramakria999@gmail.com>
2020-04-14 19:35:02 +1000

    update integration tests
    update it

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -5,7 +5,9 @@ void call(Map params) {
   //access config
   echo "Current stage config: ${params.config}"
    
- 
+  //install assemblies in jenkins local repo
+  evho "install assemblies - integration-tests *******"
+  mavenExecute script: this, goals: 'clean assembly:single'
   //execute original stage as defined in the template
  params.originalStage()
 
