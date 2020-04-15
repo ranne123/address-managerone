a577b0cc3898c6da82437b8046e7dc14bf92779e
ranne123 <ramakria999@gmail.com>
2020-04-15 14:19:53 +1000

    iupdate the groovy script
    update the groovy script

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -6,31 +6,19 @@ void call(Map params) {
   echo "Current stage config: ${params.config}"
    
   //install assemblies in jenkins local repo
-  echo "install assemblies - integration-tests *******"
+  echo "generate the  assemblies - integration-tests *******"
   mavenExecute script: this, goals: 'assembly:single'
   
   //install the assemblies into local maven repository (Docker based )
   echo "installing the assemblies ..into local repo"
-   mavenExecute script: this, goals: 'assembly:single'
-  org.apache.maven.plugins\:maven-install-plugin\:3.0.0-M1\:install-file -Dfi\
-le=target\address-manager-1.0-SNAPSHOT-integrationtest.jar  -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples \
-       -DartifactId=address-manager-integration-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=\
-integrationtest'
+   mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=target\address-manager-1.0-SNAPSHOT-integrationtest.jar  -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples         -DartifactId=address-manager-integration-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=integrationtest'
   
   //install the aaplication jar into local repo
- mavenExecute script: this, goals: 'assembly:single'
-  org.apache.maven.plugins\:maven-install-plugin\:3.0.0-M1\:install-file -Dfi\
-le=application\target\address-manager-application-applicationclasses.jar -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples \
-       -DartifactId=address-manager-application -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=\
-applicationclasses'
+ mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=application\target\address-manager-application-applicationclasses.jar -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples -DartifactId=address-manager-application -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=applicationclasses'
   
   
   //install the unit-test jar into local repo
- mavenExecute script: this, goals: 'assembly:single'
-  org.apache.maven.plugins\:maven-install-plugin\:3.0.0-M1\:install-file -Dfi\
-le=target\address-manager-1.0-SNAPSHOT-unittest.jar -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples \
-       -DartifactId=unit-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=\
-unittest'
+ mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=target\address-manager-1.0-SNAPSHOT-unittest.jar -Dversion=1.0-SNAPSHOT  -DgroupId=com.sap.cloud.s4hana.examples   -DartifactId=unit-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=unittest'
   
   
   
