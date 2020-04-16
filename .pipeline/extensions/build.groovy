d9199add09dbbdb6336d95c4161e640a09c04c7b
ranne123 <ramakria999@gmail.com>
2020-04-17 08:10:13 +1000

    x
    x

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -7,8 +7,7 @@ import com.sap.piper.ConfigurationMerger
 import com.sap.piper.Utils
 import com.sap.cloud.sdk.s4hana.pipeline.MavenUtils
 void call(Map params) {
- artifactSetVersion script: this, buildTool: 'maven', versioningTemplate: 'rama123'
- echo "print the shell env vars "
+ 
  sh  "printenv"
 
   //print envrs variables
