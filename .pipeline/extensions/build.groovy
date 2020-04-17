d9a5951f1ae5e681e1510e1464b52544df60994e
ranne123 <ramakria999@gmail.com>
2020-04-17 10:16:58 +1000

    we
    we

--- .pipeline/extensions/build.groovy
+++ .pipeline/extensions/build.groovy
@@ -42,14 +42,14 @@ void call(Map params) {
   //install the assemblies into local maven repository (Docker based )
   echo "installing the assemblies ..into local repo from unit-tests"
  
-sh "mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=target/address-manager-rama123-integrationtest.jar  -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples         -DartifactId=address-manager-integration-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=integrationtest'"
+sh "mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=target/address-manager-rama123-integrationtest.jar  -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples         -DartifactId=address-manager-integration-tests -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dclassifier=integrationtest"
  
   //install the aaplication jar into local repo
-sh  "mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=application/target/address-manager-application-applicationclasses.jar -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples -DartifactId=address-manager-application -Dversion=rama123 -Dpackaging=jar -Dclassifier=applicationclasses'"
+sh  "mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=application/target/address-manager-application-applicationclasses.jar -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples -DartifactId=address-manager-application -Dversion=rama123 -Dpackaging=jar -Dclassifier=applicationclasses"
   
   
   //install the unit-test jar into local repo
- sh "mavenExecute script: this, goals: 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile= target/address-manager-rama123-unittest.jar -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples   -DartifactId=unit-tests -Dversion=rama123 -Dpackaging=jar -Dclassifier=unittest'"
+ sh "mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile= target/address-manager-rama123-unittest.jar -Dversion=rama123  -DgroupId=com.sap.cloud.s4hana.examples   -DartifactId=unit-tests -Dversion=rama123 -Dpackaging=jar -Dclassifier=unittest"
   
   
   
